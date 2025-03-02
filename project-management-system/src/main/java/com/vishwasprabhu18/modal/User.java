5d681aec33d1196fc5d08742d70feef9cdf662db
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2025-03-02 14:12:22 +0530

    Initial Setup Backend

---
+++ project-management-system/src/main/java/com/vishwasprabhu18/modal/User.java
@@ -0,0 +1,21 @@
+package com.vishwasprabhu18.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    private int projectSize;
+}
