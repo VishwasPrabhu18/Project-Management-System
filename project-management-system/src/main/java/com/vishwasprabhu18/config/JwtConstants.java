41e4fd1b08ce8b1c389426d732dac0eb1fbe23e0
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2025-03-02 15:40:41 +0530

    Code cleaned

--- project-management-system/src/main/java/com/vishwasprabhu18/config/JwtConstants.java
+++
@@ -1,6 +0,0 @@
-package com.vishwasprabhu18.config;
-
-public class JwtConstants {
-    public static final String SECRET_KEY = "iofnsdnfionof rt3%67&*oin adfjjwpoi adw9eu 90u 9@#$on o pdphp easfa#sddadasdasd aasff234a";
-    public static final String JWT_HEADER = "Authorization";
-}
