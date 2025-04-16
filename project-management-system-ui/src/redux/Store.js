986e938b0af6a951be674fe612cab11e4261b65f
VishwasPrabhu18 <prabhuvishwas18@gmail.com>
2025-04-16 22:14:02 +0530

    code clean

--- project-management-system-ui/src/redux/Store.js
+++ project-management-system-ui/src/redux/Store.js
@@ -4,12 +4,14 @@ import { authReducer } from "./auth/Reducer";
 import { projectReducer } from "./project/Reducer";
 import { chatReducer } from "./chat/Reducer";
 import { commentReducer } from "./comment/Reducer";
+import { issueReducer } from "./issue/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
   chat: chatReducer,
   comment: commentReducer,
+  issue: issueReducer,
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
--- project-management-system-ui/src/redux/project/Reducer.js
+++ project-management-system-ui/src/redux/project/Reducer.js
@@ -1,4 +1,8 @@
-import { ACCEPT_INVITATION_REQUEST, CREATE_PROJECT_REQUEST, CREATE_PROJECT_SUCCESS, DELETE_PROJECT_REQUEST, DELETE_PROJECT_SUCCESS, FETCH_PROJECT_BY_ID_REQUEST, FETCH_PROJECT_BY_ID_SUCCESS, FETCH_PROJECTS_FAILURE, FETCH_PROJECTS_REQUEST, FETCH_PROJECTS_SUCCESS, GET_USER_REQUEST, GET_USER_SUCCESS, INVITE_TO_PROJECT_REQUEST, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_REQUEST, REGISTER_SUCCESS, SEARCH_PROJECT_SUCCESS } from "./ActionType"
+import {
+  ACCEPT_INVITATION_REQUEST, CREATE_PROJECT_REQUEST, CREATE_PROJECT_SUCCESS, DELETE_PROJECT_REQUEST,
+  DELETE_PROJECT_SUCCESS, FETCH_PROJECT_BY_ID_REQUEST, FETCH_PROJECT_BY_ID_SUCCESS,
+  FETCH_PROJECTS_REQUEST, FETCH_PROJECTS_SUCCESS, INVITE_TO_PROJECT_REQUEST, SEARCH_PROJECT_SUCCESS
+} from "./ActionType"
 
 const initialState = {
   projects: [],
@@ -21,7 +25,7 @@ export const projectReducer = (state = initialState, action) => {
         loading: true,
         error: null,
       };
-    
+
     case FETCH_PROJECTS_SUCCESS:
       return {
         ...state,
@@ -29,7 +33,7 @@ export const projectReducer = (state = initialState, action) => {
         error: null,
         projects: action.projects,
       };
-    
+
     case SEARCH_PROJECT_SUCCESS:
       return {
         ...state,
@@ -37,7 +41,7 @@ export const projectReducer = (state = initialState, action) => {
         error: null,
         searchProjects: action.projects,
       };
-    
+
     case CREATE_PROJECT_SUCCESS:
       return {
         ...state,
@@ -45,7 +49,7 @@ export const projectReducer = (state = initialState, action) => {
         error: null,
         projects: [...state.projects, action.project],
       };
-    
+
     case FETCH_PROJECT_BY_ID_SUCCESS:
       return {
         ...state,
@@ -53,7 +57,7 @@ export const projectReducer = (state = initialState, action) => {
         error: null,
         projectDetails: action.project,
       };
-    
+
     case DELETE_PROJECT_SUCCESS:
       return {
         ...state,
@@ -61,7 +65,7 @@ export const projectReducer = (state = initialState, action) => {
         error: null,
         projects: state.projects.filter((project) => project.id !== action.projectId),
       };
-    
+
     default: return state;
   }
 }
